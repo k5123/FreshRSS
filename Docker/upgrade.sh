74383035abab7d79cb2b0286ceb9008b01b7966f
k5123 <github@k512.studio>
2024-08-05 19:27:56 +0000

    commit before upstream sync

--- Docker/upgrade.sh
+++ Docker/upgrade.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "On github, merge origin into fork first. then pull. Then merge edge into mergetest locally."
+echo "On github, merge origin into fork first. then pull. Then merge edge into mergetest locally or use fork sync on GH to update mergetest branch"
